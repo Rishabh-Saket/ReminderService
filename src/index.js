43d6dea6f8624c11796a5b56a7eaaa9bbbb642df
Rishabh-Saket <87156299+Rishabh-Saket@users.noreply.github.com>
2023-10-03 18:23:39 +0530

    Added node-cron for scheduling jobs

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express=require('express');
 const bodyParser=require('body-parser');
 
+const cron=require('node-cron');
+
 const {PORT}=require('./config/serverConfig');
-const {sendBasicEmail}=require('./services/email-service');
+//const {sendBasicEmail}=require('./services/email-service');
 const setupAndStartServer = ()=> {
     const app=express();
     app.use(bodyParser.urlencoded({extended: true}));
@@ -11,12 +13,16 @@ const setupAndStartServer = ()=> {
     app.listen(PORT,()=>{
         console.log(`Server is ready to listen on port ${PORT}`);
 
-        sendBasicEmail(
+        /* sendBasicEmail(
             'support@tcs.com',
             'rssaket22@gmail.com',
             'This is a testing mail',
             'hey how are you'
-        );
+        ); */
+
+        cron.schedule('*/2 * * * *',()=>{
+            console.log("running a task every two minutes");
+        });
     });
 }
 
